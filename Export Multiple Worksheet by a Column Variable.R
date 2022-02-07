8af145ac63dfad3c74feb214f69a372d64ea6765
Deontwb <95469859+Deontwb@users.noreply.github.com>
2022-02-07 15:06:57 +0800

    update

--- Export Multiple Worksheet by a Column Variable.R
+++ Export Multiple Worksheet by a Column Variable.R
@@ -7,4 +7,4 @@ for (i in 1:length(output)) {
   writeData(wb, sheet=names(output[i]), x=output[[i]]) # Note [[]]
 }
 
-saveWorkbook(wb, "C:/Users/Dtan1/OneDrive - Williams-Sonoma Inc/Desktop/mytasks/Manual Shipment Uploading/4.Europe/Practice/For P12/Discrepancies.xlsx", overwrite = TRUE)
+saveWorkbook(wb, ".xlsx", overwrite = TRUE)
